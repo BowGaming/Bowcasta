4f49e3844e6fbb23d2eb45b57cebb5657e170649
BowGaming <109923007+BowGaming@users.noreply.github.com>
2025-10-22 14:37:21 +0200

    Create spoilerthread.py

---
+++ cogs/spoilerthread.py
@@ -0,0 +1,41 @@
+from discord.ext import commands
+from discord import Embed
+
+class SpoilerThreadCog(commands.Cog) :
+    def __init__(self, bot) :
+        self.bot = bot
+    
+        self.review_channel_id = 1430534917401477162
+    
+        # Sticky messages
+        self.thread_request_embed = Embed(
+            title="**Request Spoiler Threads**",
+            description=(
+                "Here you can request spoiler threads about new movies/shows/games! Simply state the title of whatever you want a thread for.\n" 
+                "Please ***only*** request a thread if you **intent to use it!**"
+            ),
+        )
+
+
+
+    @commands.Cog.listener()
+    async def on_message(self, message) :
+        """Checks for new messages in the spoiler thread request channel."""
+        if message.author.bot :
+            return
+        
+        if message.channel.id != self.review_channel_id :
+            return   
+        
+        # Remove previous embed messages from bot to keep latest at bottom
+        async for msg in message.channel.history(limit = 5) :
+            if msg.author == self.bot.user :
+                await msg.delete()
+            
+        # Send sticky embeds at bottom
+        await message.channel.send(embed=self.thread_request_embed)
+
+
+            
+async def setup(bot: commands.Bot) :
+    await bot.add_cog(ReviewCog(bot))
