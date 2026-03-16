202458800c0c2422bcae964539897ed6edd28ffa
BowGaming <109923007+BowGaming@users.noreply.github.com>
2026-03-16 21:26:52 +0100

    Update dakennono.py

--- cogs/dakennono.py
+++ cogs/dakennono.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
 from discord import Embed, Forbidden
 from discord import TextChannel, Thread
+import time
 import re
 
 class AkihiroCog(commands.Cog) :
@@ -19,6 +20,10 @@ class AkihiroCog(commands.Cog) :
         self.ignored_channels = {1424723487955619840,689387320142463035,1483188598688976978}
         self.ignored_forums = {1432707413181599775}
 
+        # Cooldown duration in seconds
+        self.cooldown_seconds = 60
+        self.last_executed = {}
+
 
     @commands.Cog.listener()
     async def on_message(self, message) :
@@ -35,9 +40,22 @@ class AkihiroCog(commands.Cog) :
             if message.channel.parent and message.channel.parent.id in self.ignored_forums:
                 return
             
-        if "daken" in message.content.lower():
-          await message.reply(embed=self.akihiro_message)
+        if "daken" not in message.content.lower():
+            return
 
+        channel_id = message.channel.id
+
+        # cooldown check
+        now = time.time()
+        last_time = self.last_executed.get(channel_id, 0)
+        if now - last_time < self.cooldown_seconds:
+            return  # still in cooldown
+
+        await message.reply(embed=self.akihiro_message)
+
+        # update last time used
+        self.last_executed[channel_id] = now
+        
 async def setup(bot: commands.Bot) :
     """Standard setup function for discord.py cogs."""
     await bot.add_cog(AkihiroCog(bot))
