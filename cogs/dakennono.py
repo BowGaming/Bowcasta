249367656c23be0c3ad71789570adeca6a5c08db
BowGaming <109923007+BowGaming@users.noreply.github.com>
2026-03-16 20:13:21 +0100

    Create dakennono.py

---
+++ cogs/dakennono.py
@@ -0,0 +1,23 @@
+from discord.ext import commands
+from discord import Embed, Forbidden
+import re
+
+class AkihiroCog(commands.Cog) :
+    def __init__(self, bot) :
+        self.bot = bot
+      
+        # Message
+        self.akihiro_message = Embed(
+            description=(
+                "Please do not use the name 'Daken' as this is actually a slur.\n"
+                "Instead, use one of these alternatives: Akihiro, Dark Wolverine, Hellverine, Fang."
+            ),
+        )
+
+    @commands.Cog.listener()
+    async def on_message(self, message) :
+        """Checks messages in the review channel and enforces format."""
+        if message.author.bot :
+            return
+          
+          await message.channel.send(embed=self.akihiro_message)
