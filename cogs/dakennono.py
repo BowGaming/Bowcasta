bbd400154e40ebbcba1fb2befd5ab0598e7c84f4
BowGaming <109923007+BowGaming@users.noreply.github.com>
2026-04-14 20:31:00 +0200

    Update dakennono.py

--- cogs/dakennono.py
+++ cogs/dakennono.py
@@ -63,8 +63,10 @@ class AkihiroCog(commands.Cog) :
     @commands.Cog.listener()
     async def on_message(self, message) :
 
-        pattern = re.compile(r"d\W*a\W*k\W*e\W*n"|r"d[\W_]*[a4@][\W_]*k[\W_]*[e3][\W_]*n", re.IGNORECASE)
-
+        pattern = re.compile(
+            r"(d\W*a\W*k\W*e\W*n|d[\W_]*[a4@][\W_]*k[\W_]*[e3][\W_]*n)",
+            re.IGNORECASE
+        )
         
         if not pattern.search(message.content):
             return
